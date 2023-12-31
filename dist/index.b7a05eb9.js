10317dd5ca7191c14b62d18d8fcdbaf0fa351be5
박단비 <silverwill39@gmail.com>
2023-12-31 19:46:13 +0900

    타입 단언 및 할당 단언

--- dist/index.b7a05eb9.js
+++ dist/index.b7a05eb9.js
@@ -575,145 +575,46 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"jeorp":[function(require,module,exports) {
-// 타입 종류
-// 문자
-let str;
-let red = "Red";
-let green = "Green";
-let myColor = `My color is ${red}`;
-let yourColor = "My color is" + green;
-// 숫자
-let num;
-let integer = 6;
-let float = 3.14;
-let infinity = Infinity;
-let nan = NaN;
-// 불린
-let isBoolean;
-let isDone = false;
-// Null / undefined
-let nul; // 명시적으로 아무것도 아닌 값을 지정
-let und; // 직접 사용하기 보다는 자동으로 들어가서 암시적으로 데이터가 만들어짐
-nul = null;
-console.log(nul);
-console.log(und);
-// 배열
-const fruits = [
-    "Apple",
-    "Banana",
-    "Cherry"
-];
-const numbers = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7
-];
-const union = [
-    "Apple",
-    1,
-    2,
-    "Banana",
-    3
-];
-// const array:[] = [1,2 3]
-// 객체
-// typeof DATA === 'object
-const obj = {};
-const arr = [];
-const func = function() {};
-const userA = {
-    name: "Harry",
-    age: 13,
-    isValid: true
-};
-const userB = {
-    name: "Neo",
-    age: 22,
-    isValid: false
-};
-// 함수
-// const add: (x: number, y: number) => number = function (x, y) {
-//   return x + y;
-// };
-const add = function(x, y) {
-    return x + y;
-};
-const add1_2 = add(1, 2);
-// const hello: () => void = function () {
-//   console.log("Hello world!");
-// };
-const hello = function() {
-    console.log("Hello world!");
-};
-const h = hello();
-// Any (어떤 데이터든 할당해도 상관 없다)=> 사용하지 않는 것을 권장
-let hi = "Hi!";
-hi = 123;
-hi = false;
-hi = null;
-hi = {};
-hi = [];
-hi = function() {};
-// Unknown (정확하게 어떤 데이터가 할당될지 알수 없으니까 일단은 알수없음의 상태로 두겠다)
-const a = 123;
-const u = 123;
-const any1 = a;
-const boo1 = a;
-const num1 = a;
-const arr1 = a;
-const obj1 = a;
-const any2 = u;
-// const boo2: boolean = u;
-// const num2: number = u;
-// const arr2: string[] = u;
-// const obj2: { x: string; y: string } = u;
-// Tuple => 아이템의 타입, 개수와 순서까지 명시
-const tuple = [
-    "a",
-    1,
-    false
-];
-const users = [
-    [
-        1,
-        "Neo",
-        true
-    ],
-    [
-        2,
-        "Evan",
-        false
-    ],
-    [
-        3,
-        "Lewis",
-        true
-    ]
-];
-// Void => return 키워드를 작성하지 않는 함수 안에서 반환되는 데이터의 타입
-function sayHello(msg) {
-    console.log(`Hello ${msg}`);
+// 타입 단언 (Assertion)
+// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
+// BUT 잘못 사용할 수 있기 때문에 타입 단언을 남발하는 것은 지양
+// 단언 키워드 - as (어떤 데이터가 별도의 타입이라고 단언할 때 사용)
+// Non-null 단언 연산자 - ! (null 이나 undefined가 아니다라는 것을 명시할 때만 유용)
+// 1)
+// const el = document.querySelector("body") as HTMLBodyElement;
+const el = document.querySelector("body"); // document의 querySelector 메소드를 통해 요소를 찾는데 요소를 찾지 못하면 null 데이터가 반환됨
+el.textContent = "Hello world?!"; // null일 때 textContent 속성을 사용하지 못함 => !을 사용해서 느낌표 앞의 el 변수는 절대 null이 아니다라고 단언
+const title = document.querySelector(".title");
+// 타입 가드
+if (title) title.textContent = "This is Title";
+// 2)
+// function getNumber(x: number | null | undefined) {
+//   return Number((x as number).toFixed(2));
+// }
+// function getNumber(x: number | null | undefined) {
+//   return Number(x!.toFixed(2));
+// }
+// 타입스크립트 코드 상 에러가 발생하지 않지만 getNumber에 null 데이터가 들어갈 수 있는 코드가 실제로 존재하기 때문에 잘못된 타입 단언이다 !
+// 타입 가드
+function getNumber(x) {
+    if (x) return Number(x.toFixed(2));
 }
-const greeting = sayHello("danbi");
-// Never => 절대 발생하지 않을 값
-// const nev: [] = []; 아무런 데이터를 넣을 수 없음 => 타입스크립트는 이렇게 만들어진 배열의 타입에 절대 발생할 수 없는 값이라는 never 타입을 자동으로 지정함
-const nev = [];
-nev.push(1);
-// 만약 에러 메시지에서 never 타입을 만나게 되면 타입 지정이 잘못되지 않았는지 검토해보자!
-// Union => 동시에 여러 개의 타입을 지정하고 싶을 때 유용
-let union2;
-union2 = "Hello type!";
-union2 = 123;
-union2 = false;
-const heropy = {
-    name: "Neo",
-    age: 85,
-    isValid: true
-};
+getNumber(3.1415926535);
+getNumber(null);
+// 3)
+function getValue(x, isNumber) {
+    if (isNumber) return Number(x.toFixed(2));
+    return x.toUpperCase();
+}
+console.log(getValue("hello world", false)); // "HELLO WORLD"
+console.log(getValue(3.1415926535, true)); // 3.14
+// 할당 단언 (Assertion)
+// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
+// 타입스크립트에게 이 변수가 이미 할당되어져 있는 구조니까 에러를 보여줄 필요가 없다고 딱 잘라 단언함
+// 할당 단언 키워드 - !
+let num;
+console.log(num);
+num = 123;
 
 },{}]},["d8lhj","jeorp"], "jeorp", "parcelRequire0ed1")
 
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,55 @@
-// 타입 추론 (inference)
-// "추론" - 어떠한 판단을 근거로 삼아 다른 판단을 이끌어 냄
+// 타입 단언 (Assertion)
+// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
+// BUT 잘못 사용할 수 있기 때문에 타입 단언을 남발하는 것은 지양
 
-// 타입스크립트는 타입추론 기능을 통해서 대부분의 타입을 알아서 확인할 수 있기 때문에
-// => 타입스크립트가 이해할 수 없는(추론할 수 없는) 꼭 필요한 곳에서만 type을 지정해주면 됨
+// 단언 키워드 - as (어떤 데이터가 별도의 타입이라고 단언할 때 사용)
+// Non-null 단언 연산자 - ! (null 이나 undefined가 아니다라는 것을 명시할 때만 유용)
 
-// 1) 초기화된 변수
-// 2) 기본값이 설정된 매개 변수
-// 3) 반환 값이 있는 함수
+// 1)
+// const el = document.querySelector("body") as HTMLBodyElement;
+const el = document.querySelector("body"); // document의 querySelector 메소드를 통해 요소를 찾는데 요소를 찾지 못하면 null 데이터가 반환됨
+el!.textContent = "Hello world?!"; // null일 때 textContent 속성을 사용하지 못함 => !을 사용해서 느낌표 앞의 el 변수는 절대 null이 아니다라고 단언
 
-// 초기화된 변수 'num'
-let num = 12;
+const title = document.querySelector(".title");
+// 타입 가드
+if (title) {
+  title.textContent = "This is Title";
+}
 
-// 기본값이 지정된 매개변수 'b' + 반환값이 확실한 함수 'add'
-const add = (a: number, b = 2) => a + b;
+// 2)
+// function getNumber(x: number | null | undefined) {
+//   return Number((x as number).toFixed(2));
+// }
+// function getNumber(x: number | null | undefined) {
+//   return Number(x!.toFixed(2));
+// }
+// 타입스크립트 코드 상 에러가 발생하지 않지만 getNumber에 null 데이터가 들어갈 수 있는 코드가 실제로 존재하기 때문에 잘못된 타입 단언이다 !
 
-// 타입 추론을 사용하게 되면 코드에서 타입을 선언한 부분의 내용이 최소화됨
-// BUT 타입 추론을 사용하는 것이 타입을 엄격하게 사용하는 것과 반대되는 개념은 절대 아님!!
+// 타입 가드
+function getNumber(x: number | null | undefined) {
+  if (x) {
+    return Number(x.toFixed(2));
+  }
+}
+getNumber(3.1415926535);
+getNumber(null);
+
+// 3)
+function getValue(x: string | number, isNumber: boolean) {
+  if (isNumber) {
+    return Number((x as number).toFixed(2));
+  }
+  return (x as string).toUpperCase();
+}
+console.log(getValue("hello world", false)); // "HELLO WORLD"
+console.log(getValue(3.1415926535, true)); // 3.14
+
+// 할당 단언 (Assertion)
+// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
+// 타입스크립트에게 이 변수가 이미 할당되어져 있는 구조니까 에러를 보여줄 필요가 없다고 딱 잘라 단언함
+
+// 할당 단언 키워드 - !
+
+let num!: number;
+console.log(num);
+num = 123;
