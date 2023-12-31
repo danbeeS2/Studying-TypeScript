51cf48fb65d1236430aa70959d7df1d61256c1f3
박단비 <silverwill39@gmail.com>
2023-12-31 20:14:48 +0900

    타입 가드

--- dist/index.b7a05eb9.js
+++ dist/index.b7a05eb9.js
@@ -575,46 +575,20 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"jeorp":[function(require,module,exports) {
-// 타입 단언 (Assertion)
-// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
-// BUT 잘못 사용할 수 있기 때문에 타입 단언을 남발하는 것은 지양
-// 단언 키워드 - as (어떤 데이터가 별도의 타입이라고 단언할 때 사용)
-// Non-null 단언 연산자 - ! (null 이나 undefined가 아니다라는 것을 명시할 때만 유용)
-// 1)
-// const el = document.querySelector("body") as HTMLBodyElement;
-const el = document.querySelector("body"); // document의 querySelector 메소드를 통해 요소를 찾는데 요소를 찾지 못하면 null 데이터가 반환됨
-el.textContent = "Hello world?!"; // null일 때 textContent 속성을 사용하지 못함 => !을 사용해서 느낌표 앞의 el 변수는 절대 null이 아니다라고 단언
-const title = document.querySelector(".title");
-// 타입 가드
-if (title) title.textContent = "This is Title";
-// 2)
-// function getNumber(x: number | null | undefined) {
-//   return Number((x as number).toFixed(2));
-// }
-// function getNumber(x: number | null | undefined) {
-//   return Number(x!.toFixed(2));
-// }
-// 타입스크립트 코드 상 에러가 발생하지 않지만 getNumber에 null 데이터가 들어갈 수 있는 코드가 실제로 존재하기 때문에 잘못된 타입 단언이다 !
-// 타입 가드
-function getNumber(x) {
-    if (x) return Number(x.toFixed(2));
+// 타입 가드(Guards)
+function logText(el) {
+    console.log(el.textContent);
 }
-getNumber(3.1415926535);
-getNumber(null);
-// 3)
-function getValue(x, isNumber) {
-    if (isNumber) return Number(x.toFixed(2));
-    return x.toUpperCase();
+const h1El = document.querySelector("h1");
+if (h1El instanceof HTMLHeadingElement) logText(h1El);
+function add(val) {
+    let res = "Result => ";
+    if (typeof val === "number") res += val.toFixed(2);
+    if (typeof val === "string") res += val.toUpperCase();
+    console.log(res);
 }
-console.log(getValue("hello world", false)); // "HELLO WORLD"
-console.log(getValue(3.1415926535, true)); // 3.14
-// 할당 단언 (Assertion)
-// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
-// 타입스크립트에게 이 변수가 이미 할당되어져 있는 구조니까 에러를 보여줄 필요가 없다고 딱 잘라 단언함
-// 할당 단언 키워드 - !
-let num;
-console.log(num);
-num = 123;
+add(3.141592);
+add("hello world!!");
 
 },{}]},["d8lhj","jeorp"], "jeorp", "parcelRequire0ed1")
 
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,24 @@
-// 타입 단언 (Assertion)
-// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
-// BUT 잘못 사용할 수 있기 때문에 타입 단언을 남발하는 것은 지양
+// 타입 가드(Guards)
 
-// 단언 키워드 - as (어떤 데이터가 별도의 타입이라고 단언할 때 사용)
-// Non-null 단언 연산자 - ! (null 이나 undefined가 아니다라는 것을 명시할 때만 유용)
-
-// 1)
-// const el = document.querySelector("body") as HTMLBodyElement;
-const el = document.querySelector("body"); // document의 querySelector 메소드를 통해 요소를 찾는데 요소를 찾지 못하면 null 데이터가 반환됨
-el!.textContent = "Hello world?!"; // null일 때 textContent 속성을 사용하지 못함 => !을 사용해서 느낌표 앞의 el 변수는 절대 null이 아니다라고 단언
-
-const title = document.querySelector(".title");
-// 타입 가드
-if (title) {
-  title.textContent = "This is Title";
+function logText(el: Element) {
+  console.log(el.textContent);
 }
 
-// 2)
-// function getNumber(x: number | null | undefined) {
-//   return Number((x as number).toFixed(2));
-// }
-// function getNumber(x: number | null | undefined) {
-//   return Number(x!.toFixed(2));
-// }
-// 타입스크립트 코드 상 에러가 발생하지 않지만 getNumber에 null 데이터가 들어갈 수 있는 코드가 실제로 존재하기 때문에 잘못된 타입 단언이다 !
+const h1El = document.querySelector("h1");
+if (h1El instanceof HTMLHeadingElement) {
+  logText(h1El);
+}
 
-// 타입 가드
-function getNumber(x: number | null | undefined) {
-  if (x) {
-    return Number(x.toFixed(2));
+function add(val: string | number | boolean) {
+  let res = "Result => ";
+  if (typeof val === "number") {
+    res += val.toFixed(2);
   }
-}
-getNumber(3.1415926535);
-getNumber(null);
-
-// 3)
-function getValue(x: string | number, isNumber: boolean) {
-  if (isNumber) {
-    return Number((x as number).toFixed(2));
+  if (typeof val === "string") {
+    res += val.toUpperCase();
   }
-  return (x as string).toUpperCase();
+  console.log(res);
 }
-console.log(getValue("hello world", false)); // "HELLO WORLD"
-console.log(getValue(3.1415926535, true)); // 3.14
 
-// 할당 단언 (Assertion)
-// "단언" - (개발자가 타입스크립트에게) 주저하지 아니하고 딱 잘라 말함.
-// 타입스크립트에게 이 변수가 이미 할당되어져 있는 구조니까 에러를 보여줄 필요가 없다고 딱 잘라 단언함
-
-// 할당 단언 키워드 - !
-
-let num!: number;
-console.log(num);
-num = 123;
+add(3.141592);
+add("hello world!!");
